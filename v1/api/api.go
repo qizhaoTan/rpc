7223c4938ec270885af8704821a7ec4885c1a3fd
Tan <tanqizhao1208@gmail.com>
2026-01-12 16:49:58 +0800

    refactor interface move to api.go

---
+++ v1/api/api.go
@@ -0,0 +1,24 @@
+package api
+
+import "context"
+
+// ClientConnInterface defines the functions clients need to perform unary and
+// streaming RPCs.  It is implemented by *ClientConn, and is only intended to
+// be referenced by generated code.
+type ClientConnInterface interface {
+	// Invoke performs a unary RPC and returns after the response is received
+	// into reply.
+	Invoke(ctx context.Context, method string, args any, reply any) error
+}
+
+// ServiceRegistrar wraps a single method that supports service registration. It
+// enables users to pass concrete types other than grpc.Server to the service
+// registration methods exported by the IDL generated code.
+type ServiceRegistrar interface {
+	// RegisterService registers a service and its implementation to the
+	// concrete type implementing this interface.  It may not be called
+	// once the server has started serving.
+	// desc describes the service and its methods and handlers. impl is the
+	// service implementation which is passed to the method handlers.
+	RegisterService(serverName string, impl any)
+}
--- v1/pb/hello_service.go
+++ v1/pb/hello_service.go
@@ -3,28 +3,8 @@ package pb
 import (
 	"context"
 	"fmt"
+	"v1/api"
 )
-
-// ClientConnInterface defines the functions clients need to perform unary and
-// streaming RPCs.  It is implemented by *ClientConn, and is only intended to
-// be referenced by generated code.
-type ClientConnInterface interface {
-	// Invoke performs a unary RPC and returns after the response is received
-	// into reply.
-	Invoke(ctx context.Context, method string, args any, reply any) error
-}
-
-// ServiceRegistrar wraps a single method that supports service registration. It
-// enables users to pass concrete types other than grpc.Server to the service
-// registration methods exported by the IDL generated code.
-type ServiceRegistrar interface {
-	// RegisterService registers a service and its implementation to the
-	// concrete type implementing this interface.  It may not be called
-	// once the server has started serving.
-	// desc describes the service and its methods and handlers. impl is the
-	// service implementation which is passed to the method handlers.
-	RegisterService(serverName string, impl any)
-}
 
 type ApplyHello struct {
 	Name string
@@ -38,7 +18,7 @@ type HelloClient struct {
 	Hello func(ctx context.Context, apply *ApplyHello) (*ReplyHello, error)
 }
 
-func NewHelloClient(c ClientConnInterface) *HelloClient {
+func NewHelloClient(c api.ClientConnInterface) *HelloClient {
 	helloFunc := func(ctx context.Context, apply *ApplyHello) (*ReplyHello, error) {
 		var reply ReplyHello
 		if err := c.Invoke(ctx, fmt.Sprintf("%s.%s", (*HelloClient).Name(nil), "Hello"), apply, &reply); err != nil {
@@ -56,7 +36,7 @@ func (s *HelloClient) Name() string {
 	return "hello_service"
 }
 
-func RegisterHelloServer(server ServiceRegistrar, service IHelloService) {
+func RegisterHelloServer(server api.ServiceRegistrar, service IHelloService) {
 	server.RegisterService("hello_service", service)
 }
 
